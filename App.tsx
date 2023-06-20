ed908a15f5c8e53854355c156fb091aace2391ff
MayyasNakhli <captaincharisma998@gmail.com>
2023-06-20 20:11:33 +0400

    Added background

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { queryClient } from './src/services';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Here is the birth of noon forecast</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Image source={require('./assets/background.png')} style={styles.backgroundImage} blurRadius={170}></Image>
+        <Text style={{color: "white"}}>Here is the birth of noon forecast</Text>
+      </View>
+    </QueryClientProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundImage: {
+    position: 'absolute',
+    width: "100%",
+    height: "100%"
+  }
 });
